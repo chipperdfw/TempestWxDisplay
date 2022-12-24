2f74e2bfad38d7f71485350bf2e0911777938fb2
chipperdfw <101214995+chipperdfw@users.noreply.github.com>
2022-12-23 19:48:46 -0600

    Add files via upload

---
+++ WiFi_Credentials.h
@@ -0,0 +1,5 @@
+//  You must edit this file to add the Wi-Fi Credentials for the network that has the Tempest Base unit
+
+// Set WiFi credentials
+const char ssid[] = "xxx";
+const char pass[] = "xxx";
